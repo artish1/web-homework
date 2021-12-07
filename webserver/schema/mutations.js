54b26f33cc68c9700769bd473d77def4745b29ee
Mark Artishuk <markyshuk@gmail.com>
2021-12-06 23:57:00 -0800

    Added edit transactions

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat, GraphQLList, GraphQLInputObjectType } = graphql
 const { TransactionModel } = require('../data-models/Transaction')
 const { UserModel } = require('../data-models/User')
 const { MerchantModel } = require('../data-models/Merchant')
@@ -7,22 +7,55 @@ const TransactionType = require('./transaction-type')
 const UserType = require('./user-type')
 const MerchantType = require('./merchant-type')
 
+const TransactionInputs = new GraphQLInputObjectType({
+  name: 'TransactionValues',
+  fields: {
+    user_id: { type: GraphQLString },
+    description: { type: GraphQLString },
+    merchant_id: { type: GraphQLString },
+    debit: { type: GraphQLBoolean },
+    credit: { type: GraphQLBoolean },
+    amount: { type: GraphQLFloat }
+  },
+  description: 'Values for Transaction Type'
+})
+
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     addTransaction: {
       type: TransactionType,
       args: {
-        user_id: { type: GraphQLString },
-        description: { type: GraphQLString },
-        merchant_id: { type: GraphQLString },
-        debit: { type: GraphQLBoolean },
-        credit: { type: GraphQLBoolean },
-        amount: { type: GraphQLFloat }
+        values: { type: TransactionInputs }
       },
-      /* eslint-disable-next-line camelcase */
-      resolve(parentValue, { user_id, description, merchant_id, debit, credit, amount }) {
+      resolve(
+        parentValue,
+        {
+          values: { user_id, description, merchant_id, debit, credit, amount }
+        }
+      ) {
         return new TransactionModel({ user_id, description, merchant_id, debit, credit, amount }).save()
+      }
+    },
+    removeTransaction: {
+      type: GraphQLList(TransactionType),
+      args: {
+        transaction_id: { type: GraphQLString }
+      },
+      async resolve(parent, { transaction_id }) {
+        await TransactionModel.findByIdAndDelete(transaction_id)
+        return TransactionModel.find()
+      }
+    },
+    editTransaction: {
+      type: TransactionType,
+      args: {
+        transaction_id: { type: GraphQLString },
+        values: { type: TransactionInputs }
+      },
+
+      resolve(parent, { transaction_id, values }) {
+        return TransactionModel.findByIdAndUpdate(transaction_id, values, { new: true })
       }
     },
     addUser: {
