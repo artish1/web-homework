20d7a57b96a4f6e0829fdb6e1b46edce1aa2759e
Mark Artishuk <markyshuk@gmail.com>
2021-12-16 13:58:39 -0800

    remove unused function

--- webapp/src/components/transactions/TxTable.test.js
+++ webapp/src/components/transactions/TxTable.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, act } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { RowCells } from './TxTable'
 import { transactions } from '../../../mocks/transactions-data'
 import { Transactions } from '../../transactions/index'
